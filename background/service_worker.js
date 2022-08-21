d188e916a9a06c673488df62c12fb2a11c91ddf2
zfymoon <zfymoon@outlook.com>
2022-08-21 23:53:26 +0800

    update to v3

---
+++ background/service_worker.js
@@ -0,0 +1,35 @@
+
+
+try{
+    importScripts([
+        "common/browser-polyfill.min.js",
+        "config/config.js",
+        "common/browserDetector.js",
+        "core/utils.js",
+        "common/utils.js",
+        "common/eventBus.js",
+        "common/messages.js",
+        "common/i18nManager.js",
+        "common/extensioni18nManager.js",
+        "common/languageManager.js",
+        "common/storageController.js",
+        "common/extensionStorageController.js",
+        "common/environmentAdapter.js",
+        "common/extensionEnvironmentAdapter.js",
+        "common/thunderbird.js",
+        "common/extension-init.js",
+        "common/tracker.js",
+        "core/GraphemeSplitter.js",
+        "core/Checker.js",
+        "background/flappy/flappy.data.js",
+        "background/flappy/flappy.auth.js",
+        "background/flappy/flappy.adapter.js",
+        "background/dictionarySync.js",
+        "background/synonyms.js",
+        "background/phrases.js",
+        "background/extension-main.js"
+    ])
+}catch (e) {
+    console.log("importScript "+e.message)
+}
+
